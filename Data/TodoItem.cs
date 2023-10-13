48a079f12e6c58a8e5f032beadb118f54eb1e783
rachel.allen1 <rachel.allen1@ENGR2JCP5S3>
2023-10-12 19:27:36 -0600

    Got through the todo...number 6 also doesn't have the code for controllers, so moving on to #7 which seems to be an entirely different code base

---
+++ Data/TodoItem.cs
@@ -0,0 +1,6 @@
+﻿public class TodoItem
+{
+    public string? Title { get; set; }
+    public bool IsDone { get; set; }
+}
+//https://learn.microsoft.com/en-us/aspnet/core/blazor/tutorials/build-a-blazor-app?view=aspnetcore-7.0
